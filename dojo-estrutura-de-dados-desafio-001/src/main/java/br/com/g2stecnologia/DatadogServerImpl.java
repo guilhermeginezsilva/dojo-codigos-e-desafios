7d0b050819ba2f5e0cdcd7991523fc36b71fffe5
Guilherme Ginez da Silva <guilherme.ginez@mercadolivre.com>
2020-09-18 14:10:38 -0300

    .

---
+++ dojo-estrutura-de-dados-desafio-001/src/main/java/br/com/g2stecnologia/DatadogServerImpl.java
@@ -0,0 +1,16 @@
+package br.com.g2stecnologia;
+
+import java.time.LocalDateTime;
+
+public class DatadogServerImpl implements DatadogServer{
+
+    @Override
+    public void incrementCounter() {
+
+    }
+
+    @Override
+    public void getCounter(LocalDateTime from, LocalDateTime until) {
+
+    }
+}
